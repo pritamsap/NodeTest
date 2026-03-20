ca1cafb2315755101001a7e110307c8e1e094332
pritamsap <pritamsap2@gmail.com>
2026-03-20 12:13:38 -0400

    Update folder structure

--- models/Note.js
+++ models/Note.js
@@ -1,35 +1,11 @@
-
-
-
 const mongoose = require('mongoose')
-const {mongo} = require("mongoose");
-//
-// if(process.argv.length < 3) {
-//     console.log("give password as an argument")
-//     process.exit()
-// }
-// const password = process.argv[2];
-
-
-const url = process.env.MONGODB_URL
-
-mongoose.set('strictQuery', false)
-mongoose.connect(url, {family: 4})
-    .then(result => {
-        console.log("Connected to MongoDB");
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDb', error.message)
-    })
-
 
 const noteSchema = new mongoose.Schema({
-    content:
-        {
-            type: String,
-            minLength: 5,
-            required: true
-        },
+    content: {
+        type: String,
+        required: true,
+        minlength: 5
+    },
     important: Boolean,
 })
 
@@ -40,24 +16,5 @@ noteSchema.set('toJSON', {
         delete returnedObject.__v
     }
 })
-const Note = mongoose.model('Note', noteSchema)
 
-
-// const note = new Note({
-//     content: 'HTML is easy',
-//     important: true,
-// })
-
-// Note.find({}).then(result => {
-//     result.forEach(note => {
-//         console.log(note)
-//     })
-//     mongoose.connection.close()
-// })
-
-
-// note.save().then(result => {
-//     console.log('note saved!')
-//     mongoose.connection.close()
-// })
-module.exports = Note;
+module.exports = mongoose.model('Note', noteSchema)
